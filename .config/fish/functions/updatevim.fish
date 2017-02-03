3e307c43264bbe1400bc64c9858a79e47c2fbc23
Arun Allamsetty <arun.allamsetty@gmail.com>
2017-02-03 16:17:24 -0700

    Intructions for fresh vundle install.

--- .config/fish/functions/updatevim.fish
+++ .config/fish/functions/updatevim.fish
@@ -1,5 +1,8 @@
 # Non POSIX Shell workaround for Vundle
 # https://github.com/gmarik/Vundle.vim/wiki#faq4
+#
+# For a fresh start run the following first.
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 function updatevim
   set -lx SHELL (which sh)
