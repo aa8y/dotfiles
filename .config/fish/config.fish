f27d2aac631a475dac07e70decbd10977ff2a0eb
Arun Allamsetty <arun.allamsetty@gmail.com>
2017-05-22 11:21:49 -0600

    Add Scala env vars to config.fish.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,26 @@
+# Flink setup
+set -x FLINK_SCALA_VERSION 2.10
+set -x FLINK_VERSION 1.2.0
+set -x FLINK_HOME $HOME/bin/flink/scala-$FLINK_SCALA_VERSION/$FLINK_VERSION
+
+# Path for all the go code
+set -x GOPATH $HOME/code/gocode
+
 # https://github.com/passcod/nvm-fish-wrapper
 set -x NVM_DIR ~/.nvm
 source $HOME/.config/fish/nvm-wrapper/nvm.fish
 
-# Path for all the go code
-set -x GOPATH $HOME/code/gocode
+# Scala setup
+set -x SCALA_VERSION latest
+set -x SCALA_HOME $HOME/bin/scala/$SCALA_VERSION
 
 # Spark setup
 set -x SPARK_VERSION 2.1.0
 set -x SPARK_HOME $HOME/bin/spark/$SPARK_VERSION
 set -x PYTHONPATH $SPARK_HOME/python $PYTHONPATH
 
-# Flink setup
-set -x FLINK_SCALA_VERSION 2.10
-set -x FLINK_VERSION 1.2.0
-set -x FLINK_HOME $HOME/bin/flink/scala-$FLINK_SCALA_VERSION/$FLINK_VERSION
+# Update PATH to include Go, Rust and Scala Lang binaries.
+set -gx PATH $GOPATH/bin $HOME/.cargo/bin $SCALA_HOME/bin $PATH
 
 # Set default editor to Vim
 set -x EDITOR vim
